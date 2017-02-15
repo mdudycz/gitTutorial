71f095424acaee2ffe6b0e7de0228104230b724e
Matthew <mdudycz@uwo.ca>
2017-02-15 18:06:35 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by MADMAN-ASUS on 2017-02-15.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
